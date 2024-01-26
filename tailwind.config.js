72e4637ef4e5061036bd9caebb815d5682ae1694
nabilabdullah <nabil.abdullah28@gmail.com>
2024-01-26 20:28:02 +0700

    First Commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html'],
+  theme: {
+    // container: {
+    //   center: true,
+    //   padding: '16px',
+    // },
+    extend: {
+      colors: {
+        primary: '#116466',
+        secondary: '#D1E8E2',
+        dark: '#2C3531',
+        info: '#D9B08C',
+      },
+      // screens: {
+      //   '2xl': '1320px',
+      // },
+    },
+  },
+  plugins: [],
+}
+
