101834d6ab20a0b3188a0cbe942d0405473eb0c8
nabilabdullah <nabil.abdullah28@gmail.com>
2024-01-26 21:29:01 +0700

    Update View

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,11 +2,18 @@
 window.onscroll = function() {
     const header = document.querySelector('header');
     const fixedNav = header.offsetTop; // = 0
+    const toTop = document.querySelector('#to-top');
 
     if (window.pageYOffset > fixedNav) {
         header.classList.add('navbar-fixed');
+
+        toTop.classList.remove('hidden');
+        toTop.classList.add('flex');
     }else{
         header.classList.remove('navbar-fixed');
+
+        toTop.classList.remove('flex');
+        toTop.classList.add('hidden');
     }
 }
 
@@ -17,4 +24,12 @@ const navMenu = document.querySelector('#nav-menu');
 humberger.addEventListener('click', function () {
     humberger.classList.toggle('humberger-active');
     navMenu.classList.toggle('hidden'); 
+});
+
+// Close humberger menu in window click
+window.addEventListener('click', function(e) {
+    if(e.target != humberger && e.target != navMenu){
+        humberger.classList.remove('humberger-active');
+        navMenu.classList.add('hidden'); 
+    }
 });
